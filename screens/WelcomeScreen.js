10392777e781672c50c46840418c17357b12993d
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-08-22 13:47:28 +0300

    loading logic for skipping the welcome screen added.

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,14 +1,32 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
 import Slides from '../components/Slides';
 import SLIDES_DATA from '../data/welcomeData';
 
 
 class WelcomeScreen extends Component {
+  state = { token: null }
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem('fb_token');
+    if (token) {
+      this.props.navigation.navigate('map');
+      this.setState({ token });
+    } else {
+      this.setState({ token: false });
+    }
+    this.setState({ token });
+  }
   onSlideComplete = () => {
     this.props.navigation.navigate('auth');
   }
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
+    console.log(this.state.token);
     return (
       <Slides data={SLIDES_DATA.data} onComplete={this.onSlideComplete} />
     );
