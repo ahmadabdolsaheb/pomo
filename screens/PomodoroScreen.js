2ea2e2b9175e53456aca2cb2b16204a6785afa79
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-10-02 14:52:56 +0300

    timer face added

--- screens/PomodoroScreen.js
+++ screens/PomodoroScreen.js
@@ -4,6 +4,10 @@ import { Icon } from 'react-native-elements';
 import * as d3 from 'd3';
 
 const { Surface, Group, Shape } = ART;
+const smallerDimention = Dimensions.get('window').height < Dimensions.get('window').width
+  ? Dimensions.get('window').height :
+  Dimensions.get('window').width;
+const dimention = smallerDimention * 0.9;
 
 class PomodoroScreen extends Component {
 
@@ -31,10 +35,7 @@ class PomodoroScreen extends Component {
         color: '#FFFFFF'
       }
     ];
-    const smallerDimention = Dimensions.get('window').height < Dimensions.get('window').width
-      ? Dimensions.get('window').height :
-      Dimensions.get('window').width;
-    const dimention = smallerDimention * 0.9;
+
     const sectionAngles = d3.pie().value(d => d.price)(userPurchases);
     const path = d3.arc()
     .outerRadius((dimention / 2)) //must be less than 1/2 the chart's height/width
@@ -42,6 +43,7 @@ class PomodoroScreen extends Component {
     .innerRadius((dimention / 2) * 0.96); //the size of the inner 'donut' whitespace
     return (
     <View style={styles.container}>
+      <View style={styles.items}>
         <Surface width={dimention} height={dimention} style={styles.circle}>
           <Group x={dimention / 2} y={dimention / 2}>
             {sectionAngles.map(section => (
@@ -53,7 +55,12 @@ class PomodoroScreen extends Component {
            ))}
           </Group>
         </Surface>
-
+        <View style={styles.timerContainer}>
+          <Text style={styles.timer}>
+           88:83
+          </Text>
+        </View>
+      </View>
     </View>
     );
   }
@@ -66,8 +73,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   circle: {
-    backgroundColor: 'pink'
+
   },
+  items: {
+    width: dimention,
+    height: dimention,
+
+  },
+  timerContainer: {
+    position: 'absolute',
+    width: 138,
+    height: 60,
+    top: (dimention / 2) - 30,
+    left: (dimention / 2) - 69
+  },
+  timer: {
+    fontSize: 50,
+    color: '#333333'
+  }
+
 });
 
 export default PomodoroScreen;
